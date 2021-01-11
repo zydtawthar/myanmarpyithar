efbca1c2b755c07f90f5702841d562490580dfb4
zydtawthar <50417866+zydtawthar@users.noreply.github.com>
2021-01-11 12:13:24 +0630

    Update myanmarpyithar.sh

--- myanmarpyithar.sh
+++ myanmarpyithar.sh
@@ -1,9 +1,10 @@
 #! /bin/bash
 printf "Installing Linux RDP Please Wait... " >&2
 {
-sudo useradd -m MyanmarPyiThar
-sudo adduser MyanmarPyiThar sudo
-echo 'MyanmarPyiThar:123456' | sudo chpasswd
+sudo useradd -m ThanHtikeSwe
+sudo adduser ThanHtikeSwe sudo
+usermod -aG sudo ThanHtikeSwe
+echo 'ThanHtikeSwe:ths' | sudo chpasswd
 sed -i 's/\/bin\/sh/\/bin\/bash/g' /etc/passwd
 sudo apt-get update
 wget https://dl.google.com/linux/direct/chrome-remote-desktop_current_amd64.deb
@@ -18,14 +19,14 @@ wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo dpkg --install google-chrome-stable_current_amd64.deb
 sudo apt install --assume-yes --fix-broken
 sudo apt install nautilus nano -y 
-sudo apt install qbittorrent
-sudo adduser MyanmarPyiThar chrome-remote-desktop
+sudo apt install qbittorrent nano -y
+sudo adduser ThanHtikeSwe chrome-remote-desktop
 } &> /dev/null &&
 printf "\nSetup Complete " >&2 ||
 printf "\nError Occured " >&2
 printf '\nCheck https://remotedesktop.google.com/headless  Copy Command Of Debian Linux And Paste Down\n'
 read -p "Paste Here: " CRP
-su - MyanmarPyiThar -c """$CRP"""
+su - ThanHtikeSwe -c """$CRP"""
 printf 'Access Your RDP At https://remotedesktop.google.com/access/ \n\n'
 if sudo apt-get upgrade &> /dev/null
 then
