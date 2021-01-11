e8c3e682d57c04c17a6936bd54e196739f9cf1e9
zydtawthar <50417866+zydtawthar@users.noreply.github.com>
2021-01-11 10:22:54 +0630

    Create myanmarpyithar.sh

---
+++ myanmarpyithar.sh
@@ -0,0 +1,35 @@
+#! /bin/bash
+printf "Installing Linux RDP Please Wait... " >&2
+{
+sudo useradd -m MyanmarPyiThar
+sudo adduser MyanmarPyiThar sudo
+echo 'MyanmarPyiThar:123456' | sudo chpasswd
+sed -i 's/\/bin\/sh/\/bin\/bash/g' /etc/passwd
+sudo apt-get update
+wget https://dl.google.com/linux/direct/chrome-remote-desktop_current_amd64.deb
+sudo dpkg --install chrome-remote-desktop_current_amd64.deb
+sudo apt install --assume-yes --fix-broken
+sudo DEBIAN_FRONTEND=noninteractive \
+apt install --assume-yes xfce4 desktop-base
+sudo bash -c 'echo "exec /etc/X11/Xsession /usr/bin/xfce4-session" > /etc/chrome-remote-desktop-session'  
+sudo apt install --assume-yes xscreensaver
+sudo systemctl disable lightdm.service
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo dpkg --install google-chrome-stable_current_amd64.deb
+sudo apt install --assume-yes --fix-broken
+sudo apt install nautilus nano -y 
+sudo apt install qbittorrent
+sudo adduser MyanmarPyiThar chrome-remote-desktop
+} &> /dev/null &&
+printf "\nSetup Complete " >&2 ||
+printf "\nError Occured " >&2
+printf '\nCheck https://remotedesktop.google.com/headless  Copy Command Of Debian Linux And Paste Down\n'
+read -p "Paste Here: " CRP
+su - MyanmarPyiThar -c """$CRP"""
+printf 'Access Your RDP At https://remotedesktop.google.com/access/ \n\n'
+if sudo apt-get upgrade &> /dev/null
+then
+    printf "\n\nUpgrade Completed " >&2
+else
+    printf "\n\nError Occured " >&2
+fi
